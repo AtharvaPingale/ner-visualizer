3d738dc99830ad3ef94f903f6a9ff2b6e489b1f7
Atharva Pingale <atharvapingale@gmail.com>
2026-05-24 21:06:15 -0400

    deploying to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/ner-visualizer/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["@xenova/transformers", "onnxruntime-web", "onnxruntime-common"],
